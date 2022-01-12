e66d736c6be15c48c1db582d0d3d3ef2a787a5ae
Oleksii Kot <akkot08@gmail.com>
2022-01-12 15:06:27 +0200

    mod: Refactoring

--- src/main/java/org/example/Task3/App.java
+++ src/main/java/org/example/Task3/App.java
@@ -4,7 +4,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContextTask3.xml");
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContextTask3.xml");
 
         User user = context.getBean("user", User.class);
         System.out.println(user);
